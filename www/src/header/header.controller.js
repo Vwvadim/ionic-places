65078cc1c7e2bf7ba2c600890bdd60c11999389b
Vadim Wichmann <Vadim@MacBook-Pro-de-Vadim.local>
2015-09-09 16:38:20 +0200

    step-02

--- www/src/header/header.controller.js
+++ www/src/header/header.controller.js
@@ -4,8 +4,11 @@ angular.module('places')
 	.controller('HeaderCtrl', function($rootScope,$scope,$state)
 	{
 		$scope.title="";
+		
 		$rootScope.$on('$stateChangeSuccess',
+			
 			function(event, toState, toParams, fromState, fromPraram){
+				
 				$scope.title=toState.data.customdata2;
 			});
 	});
